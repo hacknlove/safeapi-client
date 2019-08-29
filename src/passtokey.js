85c856d991e16324fef756db0a4631a2ee26bd91
antonio <antonio@pykiss.com>
2019-08-29 10:43:16 +0200

    isometric btoa

--- src/passtokey.js
+++ src/passtokey.js
@@ -1,5 +1,13 @@
 const seedrandom = require('seedrandom')
 
+var mybtoa
+
+if (process.browser) {
+  mybtoa = btoa
+} else {
+  mybtoa = text => Buffer.from(text).toString('base64')
+}
+
 function passtokey (password) {
   const rnd = seedrandom(password)
   const buffer = new ArrayBuffer(32)
@@ -8,7 +16,7 @@ function passtokey (password) {
   for (let i = 32; --i;) {
     view[i] = rnd.int32() % 256
   }
-  return btoa(String.fromCharCode.apply(null, view))
+  return mybtoa(String.fromCharCode.apply(null, view))
 }
 
 exports.passtokey = passtokey
